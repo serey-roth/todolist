d44c1413757ffdaf8d9fa4c8ee2732f9dad0cbe9
serey_roth <roths7@wwu.edu>
2022-07-24 13:19:16 -0700

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './style/style.css';
-import { monitorAuthState } from './modules/authentication';
-import { setUpApp } from './modules/app';
-import { getThisWeekDates } from './modules/todolist';
-
-setUpApp();
-monitorAuthState();
-
-console.log(getThisWeekDates());
